775256bbd22f2e8c77b529e612ec8dd7d20cc8f1
samuel aniekan <samuelaniekan680@gmail.com>
2022-10-13 08:56:26 +0100

    modified

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -50,16 +50,28 @@ export default class AdminController {
       const { id } = req.params;
       let userId = parseInt(id);
       if (cache.has(userId)) {
-        console.log("Hit the cache");
-        return res.status(200).json({ user: cache.get(id) });
+        return res.status(200).json(cache.get(id));
       }
       const user = await this.userServices.userRepository.findOne({
         where: { id: userId },
       });
       if (!user) return res.status(404).json("User not found");
-      cache.set(userId, user, 5000);
-      console.log("Hit the db");
-      return res.status(201).json({ user: user });
+      let totalSent = 0;
+      let totalRecieved = 0;
+      const trx = await this.userServices.txtRepository.find({
+        where: { user: { id: user.id } },
+      });
+
+      trx.forEach((t) => {
+        if (t.mode === "send") totalSent += +t.amount;
+        else totalRecieved += +t.amount;
+      });
+
+      let context = { user, totalRecieved, totalSent };
+
+      cache.set(userId, context, 5000);
+
+      return res.status(201).json(context);
     } catch (error) {
       console.log(error);
       return res.status(500).json("Internal server error");
