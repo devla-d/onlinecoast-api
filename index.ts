674563293600f24fff265735d48f1356dcc434e3
samuel aniekan <samuelaniekan680@gmail.com>
2022-10-14 06:24:23 +0100

    modified

--- index.ts
+++ index.ts
@@ -3,9 +3,6 @@ import { AppDataSource } from "./src/config/db.config";
 
 AppDataSource.initialize()
   .then(() => {
-    // console.log(
-    //   "Here you can setup and run express / fastify / any other framework."
-    // );
     App.listen();
   })
   .catch((error) => console.log(error));
